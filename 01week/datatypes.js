9ac393979f17a6fb4aa0e41e6de752f427f87bd8
worthingtonml <worthington.ml@gmail.com>
2017-10-16 23:57:35 -0500

    changes to datatypes

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -36,39 +36,41 @@ type();
 
 //create function add that takes 2 numbers
 //return the sum of the two numbers passed
-function add(num1, num2){
-  return num1 + num2;
+function add(num1, num2) {
+  return num1 + num2,
+    console.log('line 41');
 }
-
 add(1, 2);
 
 // Create a function that only runs if both things are true
 // Use the and && operator, that runs if both are true
 
-function areBothTrue (x, y){
-  if (x === 15 && y === 20) {
+function areBothTrue(x, y) {
+  if (x && y) {
     return "Show me the $$$ dude!";
   }
-areBothTrue(15,20)
+}
+areBothTrue(15, 20);
+
 
 // Create a function that only runs if 1 of 2 things are true
 // Use the 'or' || operator, that runs when one of two things are true
 
-function isItTrue (d, e) {
-  if (d === 10 || e === 15) {
+function isItTrue(d, e) {
+  if (d || e) {
     return 'One of two is true!';
   }
+}
 
-isItTrue(10,90);
+isItTrue(10, 4);
 
-//Create a function with 3 parameters. Create an if statement within the function with the 'not equal value/type' operator which will only run if arguements passed are not true
-function compare(d, e, f) {
-  if (d !== e && e !== f) {
+//Write a JavaScript program that runs when both things are not true.
+function compare(d, e) {
+  if (!d && !e) {
     return 'It is alive';
   } else {
     return 'It is dead';
   }
 }
 
-compare(1,2,3);
-
+compare(1, 2);
