e50e82e0f696f3daf64d36cad1280b7d85f662d4
worthingtonml <worthington.ml@gmail.com>
2017-10-31 16:48:50 -0500

    03week/towersOfHanoi.js

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -18,41 +18,37 @@ function printStacks() {
   console.log("b: " + stacks.b);
   console.log("c: " + stacks.c);
 }
-
-function movePiece() {
-  // Only allow the last position to move from one stack to another stack.
-
-
+const i = 0;
+function movePiece(startStack, endStack) {
+  //Establish length limits for each tower since each tower can only hold a max of 4. A piece only moves after passing this test. Once it passes the start stack final is removed using the pop method placed onto the end of a new stack using the push method. The numbers must be in descending order for all stacks in order to get to this point.
+  if (stacks.a.length < 5 && stacks.b.length < 5 && stacks.c.length < 5) {
+    function properMove(startStack, endStack) {
+      const lastItem = stacks.startStack.pop();
+      stacks.endStack.push(lastItem);
+      movePiece();
+    }
+  }
 }
 
-// function isLegal(tower) {
-//   // Checks if the move is legal
-//   for (i = 0; i < tower.length - 1; i++) {
-//     if (tower[i] > tower[i + 1]) {
-//       return "Is Valid";
-//     } else {
-//       return "Is not Valid";
-//     }
-//   }
-// }
-// isLegal(stacks.a);
-
-
 function checkForWin() {
-  // Checks if there is a winner
-  for (i = 0; i < stacks.c.length - 1; i++) {
-    if (stacks.c[i] > stacks.c[i + 1] && stacks.c.length === 4) {
-      return "winner";
-    }
+  //Winner must satisfy 3 tests. If the array is in descending order, and the tower length of C is equal to 4 items in the array.
+
+  if (stacks.c[i] > stacks.c[i + 1] && stacks.c.length === 4) {
+    return "You Win!";
   }
 }
 checkForWin();
 
-
 function towersOfHanoi(startStack, endStack) {
-  // Your code here
-
+  //Once the winner gets all items on the C column in descending order, the game needs to reset. The splice method will remove all items from C. The concate merges the items in C with the empty array A.
+  if (stacks.c[i] > stacks.c[i + 1] && stacks.c.length === 4) {
+    function towersOfHanoi(startStack, endStack) {
+      const endGame = stacks.c.slice(0, 4);
+      const restart = stacks.a.concat(stacks.c);
+    }
+  }
 }
+towersOfHanoi();
 
 function getPrompt() {
   printStacks();
@@ -66,10 +62,8 @@ function getPrompt() {
 
 getPrompt();
 
-// if I ran towersOfHanoi('a','b'), the stacks would look like (a)(4, 3, 2,) (b)(1)
-// c()
-
-// Only the top disks of (a),(b),(c) are movable at any given time.(a) (4,3) (b)(1) (c)2
-// Only the smaller disks can stack on top of larger disks (a)(), (b)(), (c)(4,3,2,1)
-// Try to break the game in your tests
-//a lot of bigger companies do tdd, it takes time. Startup companies may not because they may need to get stuff out quicker
+  // if I ran towersOfHanoi('a','b'), the stacks would look like (a)(4, 3, 2,) (b)(1)
+  // c()
+  // Only the top disks of (a),(b),(c) are movable at any given time.(a) (4,3) (b)(1) (c)2
+  // Only the smaller disks can stack on top of larger disks (a)(), (b)(), (c)(4,3,2,1)
+  // Try to break the game in your tests.
